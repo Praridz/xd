8db18e488377e085a5f4a61902ab5f793cbf09b8
Pedroridz <pedro.rios@udea.edu.co>
2020-05-02 12:24:45 -0500

    first commit

---
+++ build/precache-manifest.6155a42d88811a2c1bb80cda70cded3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ac129608d3196cf871af777cd821a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ebc86dec549240ed9c3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4094185c6e3518bcf38c",
+    "url": "/static/js/2.72cef7d2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.72cef7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebc86dec549240ed9c3",
+    "url": "/static/js/main.cbb90c55.chunk.js"
+  },
+  {
+    "revision": "2d670682775064a7cfb0",
+    "url": "/static/js/runtime-main.d4d71c51.js"
+  },
+  {
+    "revision": "8f9c3eca27060e1fe8bbda70ff4aac79",
+    "url": "/static/media/LOGO1.8f9c3eca.PNG"
+  }
+]);
